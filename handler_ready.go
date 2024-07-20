ce200be772c687ab56a5328d07da993ac306bc4e
riebi <riebisv@gmail.com>
2024-07-20 20:17:34 +0200

    Add failing test

--- handler_ready.go
+++ handler_ready.go
@@ -1,7 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"testing"
+)
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	respondWithJSON(w, http.StatusOK, map[string]string{"status": "ok"})
 }
+
+func TestReadiness(t *testing.T) {
+	got := 8
+	if got != 8 {
+    t.Fatalf("Foo %v, got: %v", 8, got)
+	}
+}
+
---
+++ super_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSomething(t *testing.T) {
+	got := 8
+	want := 8
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("Beeeer")
+	}
+}
+
+func TestWront(t *testing.T) {
+	t.Fatalf("It should fail")
+}
